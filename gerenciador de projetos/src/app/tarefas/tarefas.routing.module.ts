9367259bea47a6c0391e621334d9add1723fa90c
Marcos <enzojbnss@gmail.com>
2018-10-11 18:12:30 -0300

    projeto completo

---
+++ gerenciador de projetos/src/app/tarefas/tarefas.routing.module.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { ListarTarefasComponent } from './listar';
+import { CadastrarTarefasComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+
+export const TarefasRoutes: Routes =[
+  {
+  	path : 'tarefas',
+  	redirectTo : 'tarefas/listar'
+  },
+  {
+  	path : 'tarefas/listar',
+  	component : ListarTarefasComponent
+  },
+  {
+  	path : 'tarefas/cadastrar',
+  	component : CadastrarTarefasComponent
+  },
+  {
+  	path : 'tarefas/editar/:id',
+  	component : EditarTarefaComponent
+  }
+ ];
